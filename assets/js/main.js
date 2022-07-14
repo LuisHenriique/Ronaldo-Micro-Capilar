ce7ab23274756ef497b97ca94ed3fa5e9d45b8b6
Luis Henriique <luishenrique162004@gmail.com>
2022-07-13 22:53:52 -0300

    adding effects javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,9 @@
-// não é um numero  =  num
-// divisilvel por 3 &  5  = fizzBUzz
-// divisivel por 3 = fizz
-// divisivel por 5  = buzz
-// não divisilvel  = num
+const slider = document.querySelector('.slider input')
+const img = document.querySelector('.images .img-2')
+const dragLine = document.querySelector('.slider .drag-line')
 
-const numero = 5
-function fizzBUzz() {
-  if (typeof numero != 'number') {
-    return console.log('Não é um numero')
-  }
-  if (numero === 0) {
-    return console.log('valor invalido')
-  }
-  if (numero % 3 === 0 && numero % 5 === 0) {
-    return console.log('FizzBUzz')
-  }
-  if (numero % 3 === 0) {
-    return console.log('Fizz')
-  }
-  if (numero % 5 === 0) {
-    return console.log('BUzz')
-  } else {
-    console.log(numero)
-  }
+slider.oninput = () => {
+  let sliderVal = slider.value
+  dragLine.style.left = sliderVal + '%'
+  img.style.width = sliderVal + '%'
 }
-
-fizzBUzz()
