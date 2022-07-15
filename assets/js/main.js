ad3cd2a34050f4d1f9823e1af2066f72352e3c9b
Luis Henriique <luishenrique162004@gmail.com>
2022-07-14 23:47:36 -0300

    addinting javascript in images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const slider = document.querySelector('.slider input')
-const img = document.querySelector('.images .img-2')
-const dragLine = document.querySelector('.slider .drag-line')
+const slider = document.querySelector('.slider input ')
+const dragLine = document.querySelector('.slider .drag-line ')
+const img = document.querySelector('.images .img-2 ')
 
 slider.oninput = () => {
   let sliderVal = slider.value
