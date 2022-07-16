1e7181bb2486dbff3d2cec0ac3d35c670621f7a9
Luis Henriique <luishenrique162004@gmail.com>
2022-07-15 23:11:31 -0300

    create carrousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
-const slider = document.querySelector('.slider input ')
-const dragLine = document.querySelector('.slider .drag-line ')
-const img = document.querySelector('.images .img-2 ')
+const imgs = document.querySelector('#img')
+const images = document.querySelectorAll('.images')
 
-slider.oninput = () => {
-  let sliderVal = slider.value
-  dragLine.style.left = sliderVal + '%'
-  img.style.width = sliderVal + '%'
+let carrouselIndex = 0
+
+function carrousel() {
+  carrouselIndex++
+
+  if (carrouselIndex > images.length - 1) {
+    carrouselIndex = 0
+  }
+  imgs.style.transform = `translateX(${-carrouselIndex * 41.2}rem)`
 }
+
+setInterval(carrousel, 1850)
